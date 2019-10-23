d746acb20c9f96315cd960455dc760d560803638
rajeshmurari <56710575+rajeshmurari@users.noreply.github.com>
2019-10-23 10:53:17 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,10 @@
+pi = 3.14
+name = "math"
+def welcome(name):   # welcome is the module
+    ''' it will print the name with welcome'''
+    print("Welcome "+name)
+
+def sqrt(n):        # sqrt is a module to print square root of number n
+    'it will print the sqrt of number n'
+    print(n**0.5)
+          
---
+++ math_type.py
@@ -0,0 +1,17 @@
+def addition(a,b):
+    print(a+b)
+    
+def subtraction(c,d):
+    print(c-d)
+
+def multiply(h,i):
+    print(h*i)
+
+def square(e):
+    print(e**2)
+
+def power(f,g):
+    print(f**g)
+    
+
+    
